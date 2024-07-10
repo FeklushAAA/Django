afb91ea77eb56c7719322757c4e213aee7714850
Marsel <99655240+FeklushAAA@users.noreply.github.com>
2024-07-10 11:40:51 +0300

    Second_lesson
    Добавил макеты index и products вместе со стилями

--- store-server/store/store/urls.py
+++ store-server/store/store/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+from products.views import index, products
+
 #Набор уникальных путей к именам файлов для перехода на отдельные страницы
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='Index'),
+    path('products/', products, name='Products'),
 ]
